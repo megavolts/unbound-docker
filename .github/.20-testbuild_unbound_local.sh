8854999201f03e69f54e742cb09794ad89308024
Marc Oggier <marc.oggier@megavolts.ch>
2026-02-27 15:47:14 -0900

    Minor fix

--- .github/.20-testbuild_unbound_local.sh
+++ .github/.20-testbuild_unbound_local.sh
@@ -4,26 +4,32 @@ UNBOUND_VERSION=$(cat buildvars | grep UNBOUND_VERSION  | cut -d= -f2)
 OPENSSL_DOCKERBUILDENV_VERSION=$(cat buildvars | grep OPENSSL_DOCKERBUILDENV_VERSION  | cut -d= -f2)
 DOCKER_IMAGE_VERSION=$(cat buildvars | grep DOCKER_IMAGE_VERSION  | cut -d= -f2)
 
-# TODO check if REVISION EXISTS
-# Check if BUILD_VERSION is already released
+ # Check if BUILD_VERSION is already released
 # Check for latest released
 if [[ $(curl -s https://api.github.com/repos/megavolts/$APP-docker/releases | grep "status" | cut -d\" -f4) = 404 ]];
     then
     echo "No release existing"
 else
     LATEST_RELEASE=$(curl -s https://api.github.com/repos/megavolts/$APP-docker/releases | jq '.[0] | .name' -r)
-
-    if [[ $LATEST_RELEASE = $BUILD_IMAGE_VERSION ]];
+    BUILD_VERSION=$( echo $DOCKER_IMAGE_VERSION | cut -d- -f1)
+    BUILD_REVISION=$( echo $DOCKER_IMAGE_VERSION | cut -d- -f2)
+    if [[ $LATEST_RELEASE = $DOCKER_IMAGE_VERSION ]];
         then
-        echo "Release $BUILD_IMAGE_VERSION already exist. Updated build version with next revision"
-        BUILD_VERSION=$( echo $BUILD_IMAGE_VERSION | cut -d- -f1)
-        BUILD_REVISION=$( echo $BUILD_IMAGE_VERSION | cut -d- -f2)
+        echo "Release $DOCKER_IMAGE_VERSION already exist. Updated build version with next revision"
         BUILD_REVISION=$(( $BUILD_REVISION +1 ))
-        BUILD_IMAGE_VERSION=$BUILD_VERSION-$BUILD_REVISION
+        DOCKER_IMAGE_VERSION=$BUILD_VERSION-$BUILD_REVISION
+    else
+        echo "No new release. Rebuilding with incremented version number."
+        BUILD_REVISION=$( echo $DOCKER_IMAGE_VERSION | cut -d- -f2)
+        BUILD_REVISION=$(( $BUILD_REVISION +1 ))
     fi
+    DOCKER_IMAGE_VERSION=$BUILD_VERSION-$BUILD_REVISION
 fi
 
-echo "Building docker image $DOCKER_IMAGE_VERSION with openssl build env $OPENSSL_DOCKERBUILDENV_VERSION and unbound $UNBOUND_VERSION on $BUILD_DATE"
+echo "Building docker image $DOCKER_IMAGE_VERSION"
+echo "  - OpenSSL version: $OPENSSL_DOCKERBUILDENV_VERSION"
+echo "  - Unbound version: $UNBOUND_VERSION"
+
 
 docker buildx build ./unbound -t megavolts/unbound:local -t megavolts/unbound:latest --load \
     --build-arg BUILD_IMAGE_VERSION=$DOCKER_IMAGE_VERSION \
